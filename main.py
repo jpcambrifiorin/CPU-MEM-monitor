c69ba569e86b0f33acd714565335c6fc64cd415c
Joao Paulo C. Fiorin <jpcambrifiorin@gmail.com>
2022-07-05 18:09:00 -0300

    Create main.py
    função principal

---
+++ main.py
@@ -0,0 +1,21 @@
+from click import clear
+import psutil
+import time
+import psutil
+
+
+def MostrarUso(usoCpu,usoMemoria,barras=20):
+  porcentagem_cpu = (usoCpu/100.0)
+  porcentagem_memoria = (usoMemoria/100.0)
+  barraCpu = '█' * int(porcentagem_cpu * barras) + '-' * int(barras - int(porcentagem_cpu * barras))
+  barraMemoria = '█' * int(porcentagem_memoria * barras) + '-' * int(barras - int(porcentagem_memoria * barras))
+
+  print(f"\rCPU Uso:|{barraCpu}| {usoCpu:.2f}%  ",end="")
+  print(f"MEM Uso:|{barraMemoria}| {usoMemoria:.2f}%  ",end="\r")
+
+
+if __name__=='__main__':
+
+  while True:
+    MostrarUso(psutil.cpu_percent(),psutil.virtual_memory().percent)
+    time.sleep(1)
